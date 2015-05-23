957867ccd91c3119a6c83e7fb3dd64d75bf7db30
ernestaaron <ernest_aaron@msn.com>
2015-05-23 13:22:33 -0700

    location methods added

--- app/src/main/java/com/engineeringbits/smartrestuarant/MainActivity.java
+++ app/src/main/java/com/engineeringbits/smartrestuarant/MainActivity.java
@@ -1,15 +1,31 @@
 package com.engineeringbits.smartrestuarant;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-
 import com.google.gson.Gson;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener  {
+    private GoogleApiClient mGoogleApiClient; // declare Google Api Client Fusion Provider
+    private LocationRequest mLocationRequest; // declare the Location Request
+    private Location mCurrentLocation; // Location object
+    private static final String TAG = "MainActivity";//logging results
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,12 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "before calling createRequest");
+        createLocationRequest();// call func to create new location request
+        Log.d(TAG, "after calling createRequest");
+        buildGoogleApiClient(); // call the func fused location provider
+        Log.d(TAG, "after calling buildGoogleApiClient");
+
     }
 
     @Override
@@ -41,11 +63,56 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    //main entry point for fusion location provider
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        //start to get the location update
+        Log.d(TAG, "before calling startupdate");
+        startLocationUpdates();
+}
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    //create location request
+    protected void createLocationRequest() {
+        Log.d(TAG, "in calling createLocationRequest");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(60000);
+        mLocationRequest.setFastestInterval(60000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+    LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        updateUI();
+    }
+
     private void updateUI() {
-        //TextView mLat = (TextView) findViewById(R.id.mLatitudeText);
-        //TextView mLong = (TextView) findViewById(R.id.mLongitudeText);
-        //mLat.setText(String.valueOf(mCurrentLocation.getLatitude()));
-        //mLong.setText(String.valueOf(mCurrentLocation.getLongitude()));
-        //mLastUpdateTimeTextView.setText(mLastUpdateTime);
+        Log.d(TAG, "lat is"+String.valueOf(mCurrentLocation.getLatitude()));
+        Log.d(TAG, "long is"+String.valueOf(mCurrentLocation.getLongitude()));
     }
 }
